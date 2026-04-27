83f285f504a1f28e6ee98cce5a9b01eea7808414
dinhkhacquoc2004 <quocdkhe180045@fpt.edu.vn>
2026-04-27 13:58:38 +0700

    Initial commit

---
+++ src/main/java/com/example/babyoi_be/BabyOiBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.babyoi_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyOiBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabyOiBeApplication.class, args);
+    }
+
+}
