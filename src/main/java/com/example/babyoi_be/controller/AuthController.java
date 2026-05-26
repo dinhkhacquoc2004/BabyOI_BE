654f96be4df52bc9bfbec9e4ad535079ac125ece
dinhkhacquoc2004 <quocdkhe180045@fpt.edu.vn>
2026-05-26 11:11:42 +0700

    Revert "QuocDK-26052026-updateLinkPicture"
    This reverts commit fb0846d9ae5001e45ea2cf3fb01b4e5f6b91df08.

--- src/main/java/com/example/babyoi_be/controller/AuthController.java
+++ src/main/java/com/example/babyoi_be/controller/AuthController.java
@@ -83,10 +83,7 @@ public class   AuthController {
 
     @PostMapping("/logout")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void logout(
-            @RequestHeader("Authorization") String authorizationHeader,
-            @RequestBody(required = false) RefreshTokenRequest request
-    ) {
-        authService.logout(authorizationHeader, request);
+    public void logout(@RequestHeader("Authorization") String authorizationHeader) {
+        authService.logout(authorizationHeader);
     }
 }
